182fe47fbf9f823efd5f88e7942c96800bc3ed23
Pulavarthykarthik-nagarro <113983024+Pulavarthykarthik-nagarro@users.noreply.github.com>
2022-12-16 12:47:29 +0530

    resolved merge conflicts

--- ADB/notebooks/demonote/demo.py
+++ ADB/notebooks/demonote/demo.py
@@ -1,11 +1,11 @@
 # Databricks notebook source
 print("Hello world")
 print("Good morning")
-print("Hello Australia")
 print("Hello India")
 print("the UK")
 print("canada")
 print("hello mexico")
 print("asia")
 print("europe")
-
+print("hello america")
+print("hello worldd")
